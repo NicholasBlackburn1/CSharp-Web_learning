8f412d40d056b1d485a7de35c8d978a712d940b6
NicholasBlackburn1 <nickblackburn02@gmail.com>
2021-05-03 13:41:55 +0100

    adding Network Bandwith mon for server

---
+++ Models/ServerData.cs
@@ -0,0 +1,23 @@
+/**
+* This class is for handling the Collection and Estimation of Networking Data from the server to client
+* @author Nicholas Blackburn
+**/
+
+using System;
+
+
+
+
+
+
+
+namespace Server.Data
+{
+
+    public class ServerBandwith{
+
+        private Double uploadBandwith;
+        private Double downloadBandwith;
+        private Double pingTime;
+    } 
+}
